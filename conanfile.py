50649ef35e1e4a863f54802cea2c31dc7ed942d9
akhoroshev <arthoroshev@gmail.com>
2020-11-17 19:22:58 +0300

    refactor conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,18 @@
 from conans import ConanFile, CMake
 
+
 class Conan(ConanFile):
     url = "https://github.com/biocad/cuq"
     description = "CUDA multi-GPU concurrent tasks queue"
     settings = "os", "build_type", "arch", "compiler", "CUDA"
     generators = "cmake"
-    exports_sources="*"
+    exports_sources = "*"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+        cmake.parallel = False
         cmake.test()
 
     def package(self):
